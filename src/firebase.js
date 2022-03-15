56efb13b574dd6b4a973632073abc0a1532d4c1b
adimasmudi <adimasmudi@gmail.com>
2022-03-15 16:08:54 +0700

    initialize the repository

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgaNrcpwmj7EKYA1JafMY0MJU-SIEYQoo",
+  authDomain: "find-me-9ba71.firebaseapp.com",
+  databaseURL: "https://find-me-9ba71-default-rtdb.firebaseio.com",
+  projectId: "find-me-9ba71",
+  storageBucket: "find-me-9ba71.appspot.com",
+  messagingSenderId: "124450515769",
+  appId: "1:124450515769:web:7dc33c2c050aa2dd3216e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
